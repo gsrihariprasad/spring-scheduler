f8eee3b0f486c9ac6d1407c1a8b3dc301f7d0ecb
srihari <gsriharijava@gmail.com>
2018-07-31 17:55:32 +0530

    spring scheduler

---
+++ src/main/java/com/sriharilabs/schedulers/DemoScheduled.java
@@ -0,0 +1,30 @@
+package com.sriharilabs.schedulers;
+
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@EnableAsync
+@EnableScheduling
+@Component
+public class DemoScheduled {
+
+	
+	@Scheduled(fixedRate = 200)
+	@Async("taskScheduler1")
+	//@Scheduled(cron = "*/1 * * * * *")
+	public void show() {
+		System.out.println(new Date()+ "tis executing...."+Thread.currentThread().getName());
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
